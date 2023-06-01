5b9a39cbca22588cbd0c5b0a92722e8b7e8d5a70
arya.m <arya.m@marcopacs.com>
2023-06-02 01:09:24 +0430

    opengl compatible + clear slicing + clear shaders

--- Module 1/Chapter07/3DTextureSlicingClassification/main.cpp
+++ Module 1/Chapter07/3DTextureSlicingClassification/main.cpp
@@ -6,7 +6,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "..\src\GLSLShader.h"
 #include <fstream>
 
 #define GL_CHECK_ERRORS assert(glGetError()== GL_NO_ERROR);
@@ -37,8 +36,6 @@ glm::mat4 MV,P;
 GLuint volumeVBO;
 GLuint volumeVAO;
 
-//3D texture slicing shader
-GLSLShader shader;
 
 //maximum number of slices
 const int MAX_SLICES = 512;
@@ -288,212 +285,14 @@ void OnMouseMove(int x, int y)
 	glutPostRedisplay();
 }
 
-//function to get the max (abs) dimension of the given vertex v
-int FindAbsMax(glm::vec3 v) {
-	v = glm::abs(v);
-	int max_dim = 0;
-	float val = v.x;
-	if(v.y>val) {
-		val = v.y;
-		max_dim = 1;
-	}
-	if(v.z > val) {
-		val = v.z;
-		max_dim = 2;
-	}
-	return max_dim;
-}
 
-//main slicing function
-void SliceVolume() {
-	//get the max and min distance of each vertex of the unit cube in the viewing direction
-	float max_dist = glm::dot(viewDir, vertexList[0]);
-	float min_dist = max_dist;
-	int max_index = 0;
-	int count = 0;
-
-	for(int i=1;i<8;i++) {
-		//get the distance between the current unit cube vertex and the view vector by dot product
-		float dist = glm::dot(viewDir, vertexList[i]);
-
-		//if distance is > max_dist, store the value and index
-		if(dist > max_dist) {
-			max_dist = dist;
-			max_index = i;
-		}
-
-		//if distance is < min_dist, store the value 
-		if(dist<min_dist)
-			min_dist = dist;
-	}
-
-	//find tha abs maximum of the view direction vector
-	int max_dim = FindAbsMax(viewDir);
-
-	//expand it a little bit
-	min_dist -= EPSILON;
-	max_dist += EPSILON;
-
-	//local variables to store the start, direction vectors, lambda intersection values
-	glm::vec3 vecStart[12];
-	glm::vec3 vecDir[12];
-	float lambda[12];
-	float lambda_inc[12];
-	float denom = 0;
-
-	//set the minimum distance as the plane_dist
-	//subtract the max and min distances and divide by the total number of slices
-	//to get the plane increment
-	float plane_dist = min_dist;
-	float plane_dist_inc = (max_dist-min_dist)/float(num_slices);
-
-	//for all edges
-	for(int i=0;i<12;i++) {
-		//get the start position vertex by table lookup
-		vecStart[i] = vertexList[edges[edgeList[max_index][i]][0]];
-
-		//get the direction by table lookup
-		vecDir[i] = vertexList[edges[edgeList[max_index][i]][1]]-vecStart[i];
-
-		//do a dot of vecDir with the view direction vector
-		denom = glm::dot(vecDir[i], viewDir);
-
-		//determine the plane intersection parameter (lambda) and 
-		//plane intersection parameter increment (lambda_inc)
-		if (1.0 + denom != 1.0) {
-			lambda_inc[i] =  plane_dist_inc/denom;
-			lambda[i]     = (plane_dist - glm::dot(vecStart[i],viewDir))/denom;
-		} else {
-			lambda[i]     = -1.0;
-			lambda_inc[i] =  0.0;
-		}
-	}
-
-	//local variables to store the intesected points
-	//note that for a plane and sub intersection, we can have 
-	//a minimum of 3 and a maximum of 6 vertex polygon
-	glm::vec3 intersection[6];
-	float dL[12];
-
-	//loop through all slices
-	for(int i=num_slices-1;i>=0;i--) {
-
-		//determine the lambda value for all edges
-		for(int e = 0; e < 12; e++)
-		{
-			dL[e] = lambda[e] + i*lambda_inc[e];
-		}
-
-		//if the values are between 0-1, we have an intersection at the current edge
-		//repeat the same for all 12 edges
-		if  ((dL[0] >= 0.0) && (dL[0] < 1.0))	{
-			intersection[0] = vecStart[0] + dL[0]*vecDir[0];
-		}
-		else if ((dL[1] >= 0.0) && (dL[1] < 1.0))	{
-			intersection[0] = vecStart[1] + dL[1]*vecDir[1];
-		}
-		else if ((dL[3] >= 0.0) && (dL[3] < 1.0))	{
-			intersection[0] = vecStart[3] + dL[3]*vecDir[3];
-		}
-		else continue;
-
-		if ((dL[2] >= 0.0) && (dL[2] < 1.0)){
-			intersection[1] = vecStart[2] + dL[2]*vecDir[2];
-		}
-		else if ((dL[0] >= 0.0) && (dL[0] < 1.0)){
-			intersection[1] = vecStart[0] + dL[0]*vecDir[0];
-		}
-		else if ((dL[1] >= 0.0) && (dL[1] < 1.0)){
-			intersection[1] = vecStart[1] + dL[1]*vecDir[1];
-		} else {
-			intersection[1] = vecStart[3] + dL[3]*vecDir[3];
-		}
-
-		if  ((dL[4] >= 0.0) && (dL[4] < 1.0)){
-			intersection[2] = vecStart[4] + dL[4]*vecDir[4];
-		}
-		else if ((dL[5] >= 0.0) && (dL[5] < 1.0)){
-			intersection[2] = vecStart[5] + dL[5]*vecDir[5];
-		} else {
-			intersection[2] = vecStart[7] + dL[7]*vecDir[7];
-		}
-		if	((dL[6] >= 0.0) && (dL[6] < 1.0)){
-			intersection[3] = vecStart[6] + dL[6]*vecDir[6];
-		}
-		else if ((dL[4] >= 0.0) && (dL[4] < 1.0)){
-			intersection[3] = vecStart[4] + dL[4]*vecDir[4];
-		}
-		else if ((dL[5] >= 0.0) && (dL[5] < 1.0)){
-			intersection[3] = vecStart[5] + dL[5]*vecDir[5];
-		} else {
-			intersection[3] = vecStart[7] + dL[7]*vecDir[7];
-		}
-		if	((dL[8] >= 0.0) && (dL[8] < 1.0)){
-			intersection[4] = vecStart[8] + dL[8]*vecDir[8];
-		}
-		else if ((dL[9] >= 0.0) && (dL[9] < 1.0)){
-			intersection[4] = vecStart[9] + dL[9]*vecDir[9];
-		} else {
-			intersection[4] = vecStart[11] + dL[11]*vecDir[11];
-		}
-
-		if ((dL[10]>= 0.0) && (dL[10]< 1.0)){
-			intersection[5] = vecStart[10] + dL[10]*vecDir[10];
-		}
-		else if ((dL[8] >= 0.0) && (dL[8] < 1.0)){
-			intersection[5] = vecStart[8] + dL[8]*vecDir[8];
-		}
-		else if ((dL[9] >= 0.0) && (dL[9] < 1.0)){
-			intersection[5] = vecStart[9] + dL[9]*vecDir[9];
-		} else {
-			intersection[5] = vecStart[11] + dL[11]*vecDir[11];
-		}
-
-		//after all 6 possible intersection vertices are obtained,
-		//we calculated the proper polygon indices by using indices of a triangular fan
-		int indices[]={0,1,2, 0,2,3, 0,3,4, 0,4,5};
-
-		//using the indices, pass the intersection vertices to the vTextureSlices vector
-		for(int i=0;i<12;i++)
-			vTextureSlices[count++]=intersection[indices[i]];
-	}
-
-	//update buffer object with the new vertices
-	glBindBuffer(GL_ARRAY_BUFFER, volumeVBO);
-	glBufferSubData(GL_ARRAY_BUFFER, 0,  sizeof(vTextureSlices), &(vTextureSlices[0].x));
-}
 
 //OpenGL initialization
 void OnInit() {
 
 	GL_CHECK_ERRORS
 
-	//create a uniform grid of size 20x20 in XZ plane
-	grid = new CGrid(20,20);
 
-	GL_CHECK_ERRORS
-
-	//Load the texture slicing shader
-	shader.LoadFromFile(GL_VERTEX_SHADER, "shaders/textureSlicer.vert");
-	shader.LoadFromFile(GL_FRAGMENT_SHADER, "shaders/textureSlicer.frag");
-
-	//compile and link the shader
-	shader.CreateAndLinkProgram();
-	shader.Use();
-		//add attributes and uniforms
-		shader.AddAttribute("vVertex");
-		shader.AddUniform("MVP");
-		shader.AddUniform("volume");
-		shader.AddUniform("lut");
-
-		//pass constant uniforms at initialization
-		//we bind the volume texture to texture unit 0
-		//and the transfer function texture to texture unit 1
-		glUniform1i(shader("volume"),0);
-		glUniform1i(shader("lut"),1);
-	shader.UnUse();
-
-	GL_CHECK_ERRORS
 
 	//load volume data and generate the volume texture
 	if (is16bit? LoadVolumeUShort(): LoadVolume())
@@ -542,7 +341,6 @@ void OnInit() {
 
 //release all allocated resources
 void OnShutdown() {
-	shader.DeleteShaderProgram();
 
 	glDeleteVertexArrays(1, &volumeVAO);
 	glDeleteBuffers(1, &volumeVBO);
@@ -595,14 +393,11 @@ void OnRender() {
 
 	//bind volume vertex array object
 	glBindVertexArray(volumeVAO);
-		//use the volume shader
-		shader.Use();
-			//pass the shader uniform
-			glUniformMatrix4fv(shader("MVP"), 1, GL_FALSE, glm::value_ptr(MVP));
-				//draw the triangles
-				glDrawArrays(GL_TRIANGLES, 0, sizeof(vTextureSlices)/sizeof(vTextureSlices[0]));
-		//unbind the shader
-		shader.UnUse();
+	//use the volume shader
+	//pass the shader uniform
+	//draw the triangles
+	glDrawArrays(GL_TRIANGLES, 0, sizeof(vTextureSlices)/sizeof(vTextureSlices[0]));
+	//unbind the shader
 
 	//disable blending
 	glDisable(GL_BLEND);
@@ -638,7 +433,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitContextVersion (3, 3);
-	glutInitContextFlags (GLUT_CORE_PROFILE | GLUT_DEBUG);
+	glutInitContextFlags (GLUT_COMPATIBILITY_PROFILE| GLUT_DEBUG);
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutCreateWindow("Volume Rendering using 3D Texture Slicing - OpenGL 3.3");
 
